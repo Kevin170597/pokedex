d9643aa59f7a36d148d441495e2d91969d3a38aa
Kevin Mendoza <kevin@xymilar.com>
2023-05-19 17:13:37 -0300

    responsive grid

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -2,10 +2,9 @@ import styled from "styled-components"
 
 export const PokeContainer = styled.div`
     display: grid;
-    grid-template-columns: repeat(5, 1fr);
-    grid-auto-rows: auto;
+    grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
     gap: 10px;
-    padding: 10px 60px;
+    padding: 10px 20px;
     box-sizing: border-box;
     height: 90vh;
     overflow-y: auto;
@@ -21,5 +20,27 @@ export const PokeContainer = styled.div`
     }
     &::-webkit-scrollbar-thumb:hover {
         background: #afa153;
+    }
+
+    @media (min-width: 400px) {
+        grid-template-columns: repeat(auto-fit, minmax(150px, 1fr));
+    }
+    @media (min-width: 768px) {
+        grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+    }
+    @media (min-width: 1024px) {
+        grid-template-columns: repeat(auto-fit, minmax(210px, 1fr));
+    }
+    @media (min-width: 1280px) {
+        grid-template-columns: repeat(auto-fit, minmax(220px, 1fr));
+    }
+    @media (min-width: 1366px) {
+        grid-template-columns: repeat(auto-fit, minmax(240px, 1fr));
+    }
+    @media (min-width: 1440px) {
+        grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+    }
+    @media (min-width: 1920px) {
+        grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
     }
 `
